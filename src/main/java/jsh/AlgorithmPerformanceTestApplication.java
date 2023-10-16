631cd61afaab4e74cbeb7863cba5988b3b5088c2
Sunghwan-DS <jeonsung02@gmail.com>
2023-10-16 09:35:07 +0900

    Create AlgorithmPerformanceTest Project

---
+++ src/main/java/jsh/AlgorithmPerformanceTestApplication.java
@@ -0,0 +1,13 @@
+package jsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmPerformanceTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmPerformanceTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jsh/algorithmperformancetest/AlgorithmPerformanceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package jsh.algorithmperformancetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmPerformanceTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
